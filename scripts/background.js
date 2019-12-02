ecac35b31b8717ff3c11765393d6170d36d91e85
Svenja Weber <svenjaweber@Svenjas-MacBook-Pro.local>
2019-12-02 15:09:48 +0100

    #1: Design Sprint 02.12.19

---
+++ scripts/background.js
@@ -0,0 +1,14 @@
+ 
+ function mouseOver1() {
+    document.body.style.backgroundImage = "url('hintergründe/kalorienrechner.svg')";   
+  }
+  function mouseOut1() {
+    document.body.style.backgroundImage = "none"; 
+  }
+  
+  function mouseOver2() {
+    document.body.style.backgroundImage = "url('hintergründe/wasserrechner.svg')"; 
+  }
+  function mouseOut2() {
+    document.body.style.backgroundImage = "none"; 
+  }
