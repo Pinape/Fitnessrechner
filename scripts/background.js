2a5cea1f9459a717e3cf3e46bbd6768f04b9b1fb
Pina Peetz <pinapeetz@Pinas-MBP.fritz.box>
2019-12-17 12:48:15 +0100

    #1 Hintergründe eingefügt

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,13 @@
  
  function mouseOver1() {
-    document.body.style.backgroundImage = "url('assets/kalorienrechner.svg')";   
+  document.body.style.backgroundImage = "url('assets/home_calorie.svg')"; 
   }
   function mouseOut1() {
     document.body.style.backgroundImage = "none"; 
   }
   
   function mouseOver2() {
-    document.body.style.backgroundImage = "url('assets/wasserrechner.svg')"; 
+    document.body.style.backgroundImage = "url('assets/home_water.svg')"; 
   }
   function mouseOut2() {
     document.body.style.backgroundImage = "none"; 
