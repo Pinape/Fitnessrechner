9305a28dac3ab382e95034605647ade0f72bfd61
Johannes <johannes.munker@gmail.com>
2019-12-28 18:01:08 +0100

    #10 Buttonklick setzt Ergebnis zurück

---
+++ scripts/resetValues.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("zuruecksetzen")
+
+button.addEventListener("click", function () {
+    document.getElementById("grundumsatz").innerHTML = ""
+    document.getElementById("leistungsumsatz").innerHTML = ""
+})
