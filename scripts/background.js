d1f6191d6b05083f6b5f963745f95ede1daeb209
Andrea Thiel <andrea.thiel2@gmx.de>
2020-01-09 16:21:40 +0100

    #16 Transitions gefixed

--- scripts/background.js
+++ scripts/background.js
@@ -18,20 +18,20 @@ function mouseOut2() {
 } */
 
 document.getElementById("Kalorienrechnerhintergrund").onmouseover = function() {
-  document.getElementById("Kalorienrechnerhintergrundbild").style.display = "block";
+  document.getElementById("Kalorienrechnerhintergrundbild").style.opacity = "1";
 }
 
 document.getElementById("Kalorienrechnerhintergrund").onmouseout = function() {
-  document.getElementById("Kalorienrechnerhintergrundbild").style.display = "none";
+  document.getElementById("Kalorienrechnerhintergrundbild").style.opacity = "0";
 }
 
 
 
 
 document.getElementById("Wasserrechnerhintergrund").onmouseover = function() {
-  document.getElementById("Wasserrechnerhintergrundbild").style.display = "block";
+  document.getElementById("Wasserrechnerhintergrundbild").style.opacity = "1";
 }
 
 document.getElementById("Wasserrechnerhintergrund").onmouseout = function() {
-  document.getElementById("Wasserrechnerhintergrundbild").style.display = "none";
+  document.getElementById("Wasserrechnerhintergrundbild").style.opacity = "0";
 }
